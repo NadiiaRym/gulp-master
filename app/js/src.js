214aab4b9e7e6ef1adac7f23b779f639162971ba
NadiiaRym <46167425+NadiiaRym@users.noreply.github.com>
2019-03-06 16:51:53 +0200

    minecraft +footer

--- app/js/src.js
+++ app/js/src.js
@@ -1,1 +1,29 @@
+var $ = require('jquery');
+var _ = require('lodash');
+$('menu-opener').on('click',function(){
+  $(this).toggleClass('active')
+})
+var opener = document.querySelector('menu-opener')
+var menu = document.querySelector('.menu')
+if(opener){
+  opener.addEventListener('click',function(){
+    this.classList.toggle('actie')
+    menu.classList.toggle('actie')
+  })
+}
+
+
+// $ ('a').on('click',function(e){
+
+//   console.log(e,this)
+// })
+// $('.header').on('click',function(){
+//   console.log('header clicked !!!')
+// })
+// $(window).on('scroll',_.throttle(function(e){
+//   console.log(e)
+// }, 1000))
+// $(window).on('mousemove',_.debounce(function(e){
+//   console.log(e)
+// }, 200))
 console.log('works !!!');
